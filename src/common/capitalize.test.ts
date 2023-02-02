e5edfc8a8acec49e8087d55c56c67239dd80ceea
Héctor Matias <hector.matias@newe.eco>
2023-02-02 23:16:11 +0100

    tests added for the capitalize function

--- src/common/capitalize.test.ts
+++ src/common/capitalize.test.ts
@@ -4,4 +4,12 @@ describe('capitalize function tests', () => {
 	it('should capitalize the first letter of a string', () => {
 		expect(capitalize('hello')).toBe('Hello');
 	});
+
+	it('should return an empty string if the input is an empty string', () => {
+		expect(capitalize('')).toBe('');
+	});
+
+	it('should return an empty string if the input is not a string', () => {
+		expect(capitalize(123)).toBe(123);
+	});
 });
--- src/common/capitalize.ts
+++ src/common/capitalize.ts
@@ -1,3 +1,10 @@
 export const capitalize = (str: string) => {
+	if(!strIsValid(str)) return str;
+
 	return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+
+const strIsValid = (str: string) => {
+	return typeof str === 'string' && str.length > 0;
 };
